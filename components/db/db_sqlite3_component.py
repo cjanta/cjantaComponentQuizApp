5fe812ba8921ace202349859a668a75e59455c3e
Sven <svkanter@gmail.com>
2024-10-23 17:06:04 +0200

    erster Wurf, sqlite3 component

--- components/db/db_sqlite3_component.py
+++ components/db/db_sqlite3_component.py
@@ -3,38 +3,62 @@ import sqlite3
 
 class DB_Component_SQLite:
 
-    def __init__(self) -> None:
-        self.db = sqlite3.connect("quizapp.db")
-        self.cursor = self.db.cursor()
-        self.__create_tables()
-        self.insert_userine_values("Frida", "42")
-        self.__print_userine_table()
-        self.__close_connection()
+    table_name_userine = "userine"
+    table_name_questions_answers = "qanda"
+
+    __tableshemes = {
+        f"{table_name_userine}" : "(id INTEGER PRIMARY KEY, name text, age int)",
+        f"{table_name_questions_answers}" : "(id INTEGER PRIMARY KEY, question text, answer text)"
+    }
+    __col_descriptions = {
+        f"{table_name_userine}" : "(name, age)",
+        f"{table_name_questions_answers}" : "(question, answer)"
+    }
+
+    def __init__(self):
+        self.db = None
+        self.__check_db()
+
+    def __check_db(self):
+        if not self.db:
+            self.db = sqlite3.connect("quizapp.db")
+
+    def name_to_sheme(self,table_name :str):
+        return self.__tableshemes.get(table_name)
     
-    def __create_tables(self) -> None:
-        self.cursor.execute("DROP TABLE IF EXISTS userine")
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS userine (id INTEGER PRIMARY KEY, name text, age int)")
+    def name_to_col_description(self,table_name :str):
+        return self.__col_descriptions.get(table_name)
 
-    def insert_userine_values(self, name, age):
-        self.cursor.execute(f"INSERT INTO userine (name, age) VALUES ('{name}', '{age}')")
+    def create_table(self, table_name :str) :
+        self.__check_db()
+        c = self.db.cursor()
+        c.execute(f"DROP TABLE IF EXISTS {table_name}")
+        c.execute(f"CREATE TABLE IF NOT EXISTS {table_name} {self.name_to_sheme(table_name)}")
+        return self
+
+    def insert(self, table_name :str, value_0 :str, value_1 :str):
+        self.__check_db()
+        self.db.cursor().execute(f"INSERT INTO {table_name} {self.name_to_col_description(table_name)} VALUES ('{value_0}', '{value_1}')")
         self.db.commit()
+        return self
 
-    def __print_userine_table(self):
-        self.cursor.execute("SELECT * FROM userine")   
-        datasets = self.cursor.fetchall()
+    def read_table(self, table_name :str):
+        self.__check_db()
+        c = self.db.cursor()
+        c.execute(f"SELECT * FROM {table_name}")   
+        datasets = c.fetchall()
         for data in datasets:
             print(data) 
-
-    def __close_connection(self):
-        self.db.close()
-        self.db = None
-        self.cursor = None
-        print("Conncetion to db closed.")
-
-
-# DQL > Data Query Language
-# DML > Data Manipulation Language
-# TCL > Transaction Control Language
-# DDL > Data Definition Language
-# DCL > Data Control Language (nicht in SQLite)
-# SQL > Structured Query Language
+        return self
+    
+    def force_db_close(self):
+        if self.db:
+            self.db.close()
+            self.db = None
+            print("Conncetion to db closed by force.")
+    
+    def __del__(self):
+        if self.db:
+            self.db.close()
+            print("Conncetion to db closed.")
+        
--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from components.db.db_sqlite3_component import DB_Component_SQLite as db_component
 
 db = db_component()
+
+db_component() \
+.create_table(db.table_name_userine) \
+    .insert(db.table_name_userine,"Helga Bunterlich", 42) \
+    .insert(db.table_name_userine,"Rudi Resterampe", 77) \
+    .insert(db.table_name_userine,"Susi Zenzationalz", 24) \
+    .insert(db.table_name_userine,"Mercedes da silvio zanahoria el torro", 47) \
+    .read_table(db.table_name_userine) \
+.create_table(db.table_name_questions_answers) \
+    .insert(db.table_name_questions_answers,"Was wolle DQL ?", "Data Query Language") \
+    .insert(db.table_name_questions_answers,"Was wolle DML ?", "Data Manipulation Language") \
+    .insert(db.table_name_questions_answers,"Was wolle TCL ?", "Transaction Control Language") \
+    .insert(db.table_name_questions_answers,"Was wolle DDL ?", "Data Definition Language") \
+    .insert(db.table_name_questions_answers,"Was wolle DCL ?", "Data Control Language (nicht in SQLite)") \
+    .insert(db.table_name_questions_answers,"Was wolle SQL ?", "Structured Query Language") \
+    .read_table(db.table_name_questions_answers)
