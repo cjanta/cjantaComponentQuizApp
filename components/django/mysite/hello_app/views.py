e91eeda910f722cad7388b41799e200df56fa5d7
Sven <svkanter@gmail.com>
2024-10-24 21:17:55 +0200

    django, data into view, django+css

--- components/django/mysite/hello_app/views.py
+++ components/django/mysite/hello_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from datetime import datetime
+import random
 
 def hello_world(request):
-    return HttpResponse("Hello, World!")
+    name = 'Ben Hanter'
+    wochentag = datetime.now().strftime('%A')
+    zahl = random.randint(1,100)
+    return render(request, 'index.html', {'wochentag' : wochentag, 'name' : name, 'zahl' : zahl} )
